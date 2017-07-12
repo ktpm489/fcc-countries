210944f10a9911f33e9d2ed8a061bfd29379a510
nikrb <nik_rb@yahoo.com>
2017-07-12 10:48:43 +0100

    make nodes draggable

--- src/components/ForceGraph.js
+++ src/components/ForceGraph.js
@@ -11,11 +11,26 @@ export default class ForceGraph extends React.Component {
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
-        
+
       node
         .attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
     };
+
+    function dragstarted(d) {
+      if (!d3.event.active) force.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+    function dragended(d) {
+      if (!d3.event.active) force.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
 
     const force = d3.forceSimulation();
     force.force("charge", d3.forceManyBody());
@@ -39,14 +54,19 @@ export default class ForceGraph extends React.Component {
       .attr( 'r', 5)
       .style( 'stroke', '#999')
       .style( 'stoke-width', 1.5)
-      .style( 'fill', '#555');
+      .style( 'fill', '#555')
+      .call( d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
 
     const link = svg.selectAll( 'line')
       .data( links)
       .enter()
       .append( 'line')
       .attr( 'stroke', 'black')
-      .attr( 'stroke-width', 1);
+      .attr( 'stroke-width', 1)
+      .attr( 'stroke-opacity', 0.6);
   };
   render = () => {
     const {width,height} = this.props;
