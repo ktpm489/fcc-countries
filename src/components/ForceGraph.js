d2a8592438176923409b536ef7dece8e4ab43c83
nikrb <nik_rb@yahoo.com>
2017-07-13 16:24:19 +0100

    refactor ForceGraph

--- src/components/ForceGraph.js
+++ src/components/ForceGraph.js
@@ -7,60 +7,64 @@ import flag_sprite from './flags.png';
 
 export default class ForceGraph extends React.Component {
   componentDidMount = () => {
+    this.link = this.node = this.force = null;
+    this.init();
+  };
+  ticked = () => {
+    this.link
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+    this.node
+      .style( 'left', d => `${d.x-8}px`)
+      .style( 'top', d=> `${d.y-5}px`);
+  };
+
+  dragstarted = (d) => {
+    this.props.enableTooltip( false);
+    if (!d3.event.active) this.force.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  };
+  dragged = (d) => {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  };
+  dragended = (d) => {
+    this.props.enableTooltip( true);
+    if (!d3.event.active) this.force.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  };
+  init = () => {
     const {width, height, nodes, links} = this.props;
-
-    const ticked = () => {
-      link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-      node
-        .style( 'left', d => `${d.x-8}px`)
-        .style( 'top', d=> `${d.y-5}px`);
-    };
-
-    function dragstarted(d) {
-      if (!d3.event.active) force.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-    function dragended(d) {
-      if (!d3.event.active) force.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
-
-    const force = d3.forceSimulation();
-    force.force("charge", d3.forceManyBody());
-    force.force("link", d3.forceLink().id(function(d) { return d.index }));
-    force.force("center", d3.forceCenter(width / 2, height / 2));
+    this.force = d3.forceSimulation();
+    this.force.force("charge", d3.forceManyBody());
+    this.force.force("link", d3.forceLink().id(function(d) { return d.index }));
+    this.force.force("center", d3.forceCenter(width / 2, height / 2));
     // force.force("collide",d3.forceCollide( function(d){return d.r + 8 }).iterations(16) );
-    force.force("y", d3.forceY(0))
+    this.force.force("y", d3.forceY(0))
       .force("x", d3.forceX(0));
-    force.nodes( nodes).on("tick", ticked);
-    force.force("link").links(links);
+    this.force.nodes( nodes).on("tick", this.ticked);
+    this.force.force("link").links(links);
 
     const svg = d3.select( this.refs.graph_links);
 
-    const node = d3.select( this.refs.flags_div).selectAll( '.node')
+    this.node = d3.select( this.refs.flags_div).selectAll( '.node')
       .data( nodes)
       .enter( )
       .append( 'img')
       .attr( 'class', d => "flag flag-"+d.code)
       .call( d3.drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended))
+        .on("start", this.dragstarted)
+        .on("drag", this.dragged)
+        .on("end", this.dragended))
       .on( "mouseenter", this.props.handleMouseEnter)
       .on( "mouseleave", this.props.handleMouseLeave);
 
-    const link = svg.selectAll( 'line')
+    this.link = svg.selectAll( 'line')
       .data( links)
       .enter()
       .append( 'line')
